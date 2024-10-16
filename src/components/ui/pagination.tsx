025cf38dba95d204ebd014f152ea35dbb61907de
Maureen Martin Onetti <maureen@acchiardo.fr>
2024-10-16 23:12:38 +0200

    [FEAT] modification des boutons

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -54,7 +54,7 @@ const PaginationLink = ({ className, isActive, size = 'icon', disabled, ...props
 );
 PaginationLink.displayName = 'PaginationLink';
 
-// Pagination Previous Button
+// Pagination Button précédent
 const PaginationPrevious = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
     <PaginationLink
         aria-label="Go to previous page"
@@ -63,21 +63,20 @@ const PaginationPrevious = ({ className, ...props }: React.ComponentProps<typeof
         {...props}
     >
         <ChevronLeftIcon className="h-4 w-4" />
-        <span>Previous</span>
+        <span>Précédent</span>
     </PaginationLink>
 );
 PaginationPrevious.displayName = 'PaginationPrevious';
 
-// Pagination Next Button
+// Pagination Button next
 const PaginationNext = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
     <PaginationLink aria-label="Go to next page" size="default" className={cn('gap-1 pr-2.5', className)} {...props}>
-        <span>Next</span>
+        <span>Suivant</span>
         <ChevronRightIcon className="h-4 w-4" />
     </PaginationLink>
 );
 PaginationNext.displayName = 'PaginationNext';
 
-// Pagination Ellipsis (for skipping pages)
 const PaginationEllipsis = ({ className, ...props }: React.ComponentProps<'span'>) => (
     <span aria-hidden className={cn('flex h-9 w-9 items-center justify-center', className)} {...props}>
         <DotsHorizontalIcon className="h-4 w-4" />
